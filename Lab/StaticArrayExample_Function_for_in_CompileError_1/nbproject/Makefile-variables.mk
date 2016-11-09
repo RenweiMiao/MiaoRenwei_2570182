3d92b430bcef03d09f8d43046b65de585811378d
mikemiao1995@hotmail.com <mikemiao1995@hotmail.com>
2016-11-09 10:55:02 -0800

    Signed-off-by: mikemiao1995@hotmail.com <mikemiao1995@hotmail.com>

---
+++ Lab/StaticArrayExample_Function_for_in_CompileError_1/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=staticarrayexample_function_for_in_compileerror_1
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/staticarrayexample_function_for_in_compileerror_1
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=staticarrayexamplefunctionforincompileerror1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/staticarrayexamplefunctionforincompileerror1.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=staticarrayexample_function_for_in_compileerror_1
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/staticarrayexample_function_for_in_compileerror_1
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=staticarrayexamplefunctionforincompileerror1.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/staticarrayexamplefunctionforincompileerror1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
